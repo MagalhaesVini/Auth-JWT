81361d44db0f2f5d86d12263fa7d498f11aedd21
Vinicius Magalhaes <vinizipi@gmail.com>
2024-02-05 12:39:43 -0300

    Correções

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function checkToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(" ")[1]
 
-    if(!token) {
+    if (!token) {
         return res.status(401).json({msg: "Acesso negado!"})
     }
 
@@ -45,7 +45,6 @@ function checkToken(req, res, next) {
 
         const secret = process.env.SECRET
         jwt.verify(token, secret)
-
         next()
     } catch(error) {
         res.status(400).json({msg: "Token inválido!"})
